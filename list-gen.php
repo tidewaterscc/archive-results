7bdfb070fecf6fb4dda20393c3eb9d72dfa7d700
Allen Torres <allen.torres@forrent.com>
2016-04-04 00:39:50 -0400

    Modified script and index.

--- list-gen.php
+++ list-gen.php
@@ -4,13 +4,18 @@ error_reporting(E_ALL);
 
 $ul = '<ul>';
 function listgen($dir) {
-	$ignore = array('.', '..', '.git', '.settings', '.project', '.buildpath', '.gitignore');
+	$ignoreDir = array('.', '..', '.git', '.settings', '.project', '.buildpath', '.gitignore');
+	$ignoreFileExt = array('.xls');
 	$arr = scandir($dir);
 	$li = '';
 	foreach($arr as $i => $k) {
-		if (in_array($k, $ignore)) continue;
+		if (in_array($k, $ignoreDir)) continue;
+		if (in_array(substr($k, stripos($k, '.')), $ignoreFileExt)) continue;
+		
+		$id = str_replace(' ', '_', $k);
+		$href = (preg_match('/\.\w{3,4}$/', $k)) ? $dir . DIRECTORY_SEPARATOR . $k : '#' . $id;
 
-		$li .= '<li><a href="'. $dir . DIRECTORY_SEPARATOR . $k.'">' . $k . "</a></li>\n";
+		$li .= '<li><a id="'.$id.'" href="'. $href .'">' . $k . "</a></li>\n";
 		if (is_dir($dir . DIRECTORY_SEPARATOR . $k)) {
 			$li .= '<ul>' . listgen($dir . DIRECTORY_SEPARATOR . $k) . '</ul>';
 		}
